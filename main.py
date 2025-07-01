79a6aa7bfc61d4b9208cb802141bb2eec8a34da8
Anushka-2021 <98643007+Anushka-2021@users.noreply.github.com>
2025-07-01 12:01:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import psycopg2
 import os
 import pandas as pd
 from pathlib import Path
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, insert
 from sqlalchemy.dialects.postgresql import insert
 import time
 
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-    conn = psycopg2.connect(dbname="postgres", user="postgres", password="7527", host="127.0.0.1")
+    conn = psycopg2.connect(dbname="nfx_cp1", user="postgres", password="", host="127.0.0.1", port="5435")
     cursor = conn.cursor()
     conn.autocommit = True
 
-
     start_time = time.time()
+
     log = ["'postgres'", "'nfx_cp1'", "'insert'"]
-    sql = "INSERT INTO log_table(start_time, username, database, action) VALUES (to_timestamp(" + str(start_time) + "), " + ", ".join(str(elem) for elem in log) + ") RETURNING id;"
+    sql = "INSERT INTO \"LOGS\".log_table(start_timestamp, username, database, action) VALUES (to_timestamp(" + str(start_time) + "), " + ", ".join(str(elem) for elem in log) + ") RETURNING id;"
+    print(sql)
     cursor.execute(sql)
     log_id = cursor.fetchone()[0]
 
@@ -33,7 +34,7 @@ if __name__ == '__main__':
 
         print(dataset.columns)
         dataset.columns =dataset.columns.str.lower()
-        dataset.add_prefix("DS.")
+
         print(dataset)
         engine = create_engine('postgresql://postgres:@localhost:5435/nfx_cp1')
         table_name = Path(filename).stem
@@ -41,7 +42,7 @@ if __name__ == '__main__':
         def postgres_upsert(table, conn, keys, data_iter):
             data = [dict(zip(keys, row)) for row in data_iter]
             insert_statement = insert(table.table).values(data)
-            if(filename == 'ft_posting_f.csv' or table_name=="md_exchange_rate_d"):
+            if(filename == 'ft_posting_f.csv'):
                 upsert_statement = insert_statement
             else:
                 upsert_statement = insert_statement.on_conflict_do_update(
@@ -55,17 +56,24 @@ if __name__ == '__main__':
         else:
             dataset = dataset.drop_duplicates()
 
-        print(dataset)
-        dataset.to_sql(table_name, con=engine, if_exists="append", index=False, method=postgres_upsert)
-        print(time.time()-start_time)
+        if(table_name == "md_currency_d"):
+            dataset['currency_code'] = dataset['currency_code'].astype('Int64')
 
+        print(dataset)
+        dataset.to_sql(table_name, con=engine, if_exists="append", index=False, method=postgres_upsert, schema="DS")
+        print(time.time()-start_time)
 
 
     time.sleep(5)
 
     end_time = time.time()
-    print(end_time - start_time)
-    sql = "UPDATE log_table SET end_time = to_timestamp(" + end_time + "), duration =  " + end_time-start_time + " WHERE id = " + log_id
+    duration = end_time - start_time
+    end_time = str(end_time)
+    start_time = str(start_time)
+    duration = str(duration)
+    print(type(str(end_time)))
+
+    sql = "UPDATE \"LOGS\".log_table SET end_timestamp = to_timestamp(" + end_time + "), duration =  to_timestamp(" + end_time + ") - to_timestamp(" + start_time + ") WHERE id = " + str(log_id)
     cursor.execute(sql)
     cursor.close()
     conn.close()
