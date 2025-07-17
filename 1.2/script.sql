2f86553bad8ea6fbb7fbe7aba13cfa491056ee11
Anushka-2021 <98643007+Anushka-2021@users.noreply.github.com>
2025-07-18 00:41:08 +0300

    Update script.sql

--- 1.2/script.sql
+++ 1.2/script.sql
@@ -1,9 +1,10 @@
 -- fill "DM".dm_account_balance_f for '2017-12-31'
+
 INSERT INTO "DM".dm_account_balance_f (on_date, account_rk, balance_out, balance_out_rub)
 	SELECT on_date ,
 	account_rk, balance_out, 
 	balance_out*COALESCE(mer. reduced_cource, 1) AS balance_out_rub
 FROM "DS".ft_balance_f fbf
 	LEFT JOIN "DS".md_exchange_rate_d mer ON(mer.currency_rk = fbf.currency_rk 
-		AND ('2017-12-31' BETWEEN mer.data_actual_date AND mer.data_actual_end_date))
+		AND ('2017-12-31' BETWEEN mer.data_actual_date AND mer.data_actual_end_date+'1 day'::interval))
 WHERE on_date = '2017-12-31';
