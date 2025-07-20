1233be994d2f820604c3803f7c2c50e1fc4dd8b4
Anushka-2021 <98643007+Anushka-2021@users.noreply.github.com>
2025-07-21 01:45:12 +0300

    Update procedure.sql

--- 1.3/procedure.sql
+++ 1.3/procedure.sql
@@ -4,13 +4,13 @@ LANGUAGE 'plpgsql'
 AS $$
 
 DECLARE log_id INT;
-	from_date DATE = i_OnDate - '1 month'::interval;
-	end_date DATE = i_OnDate - '1 day'::interval;
+	i_from_date DATE = i_OnDate - '1 month'::interval;
+	i_to_date DATE = i_OnDate - '1 day'::interval;
 	
 BEGIN
 
-DELETE FROM "DM".dm_f101_round_f 
-	WHERE from_date = from_date AND end_date = end_date; 
+DELETE FROM "DM".dm_f101_round_f  
+	WHERE from_date = i_from_date AND to_date = i_to_date; 
 
 INSERT INTO "LOGS".log_table (
 	start_timestamp, 
@@ -28,42 +28,49 @@ INSERT INTO "LOGS".log_table (
 
 INSERT INTO "DM".dm_f101_round_f 
 	SELECT 
-		from_date,
-		to_date,
+		i_from_date,
+		i_to_date,
 		mlas.chapter,
 		mlas.ledger_account,
-		account_number,
 		mad.char_type AS characteristic,
 	
 		CASE WHEN mad.currency_code IN('810', '643') 
 			THEN ROUND(SUM(dabf_prev.balance_out_rub), 2)
+		ELSE 0
 		END AS balance_in_rub,
 		CASE WHEN mad.currency_code NOT IN('810', '643') 
 			THEN ROUND(SUM(dabf_prev.balance_out_rub), 2)
+		ELSE 0
 		END AS balance_in_val,
 		ROUND(SUM(dabf_prev.balance_out_rub), 2) AS balance_in_total,
 		
 		CASE WHEN mad.currency_code IN('810', '643') 
 			THEN ROUND(SUM(datf.debet_amount_rub), 2)
+		ELSE 0
 		END AS turn_deb_rub,
 		CASE WHEN mad.currency_code NOT IN('810', '643') 
 			THEN ROUND(SUM(datf.debet_amount_rub), 2)
+		ELSE 0
 		END AS turn_deb_val,
 		ROUND(SUM(datf.debet_amount_rub), 2) AS turn_deb_total,
 	
 		CASE WHEN mad.currency_code IN('810', '643') 
 			THEN ROUND(SUM(datf.credit_amount_rub), 2)
+		ELSE 0
 		END AS turn_cre_rub,
 		CASE WHEN mad.currency_code NOT IN('810', '643') 
 			THEN ROUND(SUM(datf.credit_amount_rub), 2)
+		ELSE 0
 		END AS turn_cre_val,
 		ROUND(SUM(datf.credit_amount_rub), 2) AS turn_cre_total,
 	
 		CASE WHEN mad.currency_code IN('810', '643') 
 			THEN ROUND(SUM(dabf.balance_out_rub), 2)
+		ELSE 0
 		END AS balance_out_rub,
 		CASE WHEN mad.currency_code NOT IN('810', '643') 
 			THEN ROUND(SUM(dabf.balance_out_rub), 2)
+		ELSE 0
 		END AS balance_out_val,
 		ROUND(SUM(dabf.balance_out_rub), 2) AS balance_out_total
 		
@@ -71,13 +78,13 @@ INSERT INTO "DM".dm_f101_round_f
 		LEFT JOIN "DS".md_account_d mad 
 			ON(SUBSTR(mad.account_number, 1, 5)::int = mlas.ledger_account)
 		LEFT JOIN "DM".dm_account_balance_f dabf ON(dabf.account_rk = mad.account_rk
-			AND dabf.on_date = end_date)
+			AND dabf.on_date = i_to_date)
 		LEFT JOIN "DM".dm_account_balance_f dabf_prev ON(dabf_prev.account_rk = mad.account_rk
-			AND dabf_prev.on_date = (from_date -'1 day'::interval)::date)
+			AND dabf_prev.on_date = (i_from_date -'1 day'::interval)::date)
 		FULL JOIN "DM".dm_account_turnover_f datf ON(datf.account_rk = mad.account_rk
-			AND datf.on_date BETWEEN from_date AND end_date)
-	WHERE mlas.start_date <= end_date
-		AND (mad.data_actual_end_date IS NULL OR mad.data_actual_end_date >= end_date)
+			AND datf.on_date BETWEEN i_from_date AND i_to_date)
+	WHERE mlas.start_date <= i_to_date
+		AND (mad.data_actual_end_date IS NULL OR mad.data_actual_end_date >= i_to_date)
 	GROUP BY mlas.chapter,
 		mlas.ledger_account,
 		account_number,
