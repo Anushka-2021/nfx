752c67e3115360bbfb5736b52f6c4c8f16e12ab2
Anushka-2021 <98643007+Anushka-2021@users.noreply.github.com>
2025-07-29 16:30:16 +0300

    Update scripts.sql

--- 2.3/scripts.sql
+++ 2.3/scripts.sql
@@ -32,3 +32,22 @@ SELECT *,
 	AS correct_account_out_sum
 FROM dm.account_balance_turnover
 ORDER BY account_rk, effective_date DESC;
+
+
+-- процедура 
+
+WITH cor_in AS (SELECT account_rk,
+	effective_date,
+	CASE 
+	WHEN LAG(account_out_sum) OVER(PARTITION BY account_rk ORDER BY effective_date ASC) <> account_in_sum
+		THEN LAG(account_out_sum) OVER(PARTITION BY account_rk ORDER BY effective_date ASC)
+	ELSE account_in_sum
+	END AS account_in_sum, 
+	account_out_sum
+FROM rd.account_balance)
+
+UPDATE rd.account_balance
+	SET account_in_sum = (
+		SELECT cor_in.account_in_sum
+		FROM cor_in
+		WHERE account_balance.account_rk = cor_in.account_rk AND account_balance.effective_date = cor_in.effective_date)
