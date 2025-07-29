b7f17b1310cb59b5b81eea0b18df7a58fe418ecd
Anushka-2021 <98643007+Anushka-2021@users.noreply.github.com>
2025-07-29 14:56:35 +0300

    Update script.py

--- 2.2/script.py
+++ 2.2/script.py
@@ -1,6 +1,34 @@
-В таблице rd.deal_info 6500 строк, в файле - 3500 
-  => выбираем дозагрузку с игноированием дубликатов (совпадение по всем столбцам, т. к. ключей нет)
+import psycopg2
+import os
+import pandas as pd
+from pathlib import Path
+from sqlalchemy import create_engine, insert
 
+def create_conn(db_name):
+    conn = 
+    return conn
 
-В таблице product 3500 строк, в файле - 10000 (если не принимать во внимание тот факт, что файл называется product_info, а не product)
-  => выбираем перезагрузку
+if __name__ == '__main__':
+
+    conn = psycopg2.connect(dbname="dwh", user="postgres", password="", host="127.0.0.1", port="5435")
+    cursor = conn.cursor()
+    conn.autocommit = True
+    engine = create_engine('postgresql://postgres:@localhost:5435/dwh')
+
+    #читаем данные
+    df = pd.read_csv("files_dwh\\deal_info.csv", delimiter = ",", encoding='latin-1')
+    df.columns =df.columns.str.lower()
+
+    table_name = "deal_info"
+    df = df.drop_duplicates()
+    df.to_sql(table_name, con=engine, if_exists="append", index=False, schema="rd")
+  
+
+    df = pd.read_csv("files_dwh\\product_info.csv", delimiter = ",", encoding='cp1251')
+    df.columns =df.columns.str.lower()
+    df['effective_from_date'] = pd.to_datetime(df['effective_from_date'])
+    df['effective_to_date'] = pd.to_date(df['effective_to_date'])
+
+    table_name = "product"
+    df = df.drop_duplicates()
+    df.to_sql(table_name, con=engine, if_exists="replace", index=False, schema="rd")
